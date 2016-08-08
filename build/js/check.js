eb03063d39fb28abc36b0b5ad2149d8f6f1bee9d
Valentina Kravchenko <valy.kravchenko@gmail.com>
2016-08-08 23:39:48 +0300

    more simple message function

--- build/js/check.js
+++ build/js/check.js
@@ -3,31 +3,28 @@
 function getMessage(a, b) {
   var numberOfSteps = 0;
   var distancePath = 0;
-  var message;
 
   if  (typeof a === 'boolean') {
     if (a === true) {
-      message = ('Я попал в ' + b);
+      return ('Я попал в ' + b);
       }
     else {
-      message = ('Я никуда не попал');
+      return ('Я никуда не попал');
       }
     }
   if (typeof a === 'number') {
-    message = ('Я прыгнул на ' + a * 100 + ' сантиметров');
+    return ('Я прыгнул на ' + a * 100 + ' сантиметров');
   }
-  if ((Array.isArray(a) && Array.isArray(b)) === true) {
+  if (Array.isArray(a) === true && Array.isArray(b) === true) {
     for (var i = 0; i < a.length; i++) {
       numberOfSteps += a[i] * b[i];
     }
-    message = ('Я прошёл ' + numberOfSteps + ' метров');
+    return ('Я прошёл ' + numberOfSteps + ' метров');
   }
   if (Array.isArray(a) === true && Array.isArray(b) === false) {
     for (var j = 0; j < a.length; j++) {
       distancePath += a[j];
     }
-    message = ('Я прошёл ' + distancePath + ' метров');
+    return ('Я прошёл ' + distancePath + ' метров');
   }
-
-return message;
 }
