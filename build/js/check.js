ce8d4412480a4db68cb6b20ba2e75dd97e8b2b44
Valentina Kravchenko <valy.kravchenko@gmail.com>
2016-08-16 00:21:31 +0300

    conflict fixed

--- build/js/check.js
+++ build/js/check.js
@@ -15,13 +15,13 @@ function getMessage(a, b) {
   if (typeof a === 'number') {
     return ('Я прыгнул на ' + a * 100 + ' сантиметров');
   }
-  if (Array.isArray(a) && Array.isArray(b)) {
+  if (Array.isArray(a) === true && Array.isArray(b) === true) {
     for (var i = 0; i < a.length; i++) {
       numberOfSteps += a[i] * b[i];
     }
     return ('Я прошёл ' + numberOfSteps + ' метров');
   }
-  if (Array.isArray(a)) {
+  if (Array.isArray(a) === true && Array.isArray(b) === false) {
     for (var j = 0; j < a.length; j++) {
       distancePath += a[j];
     }
